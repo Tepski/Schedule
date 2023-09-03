3f5269def9d7cfff21dfdcb69236e026db3c871c
Tepski <russeljoseph28@gmail.com>
2023-09-04 01:00:20 +0800

    Trial 1 finished

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Platform, StatusBar as SB } from "react-native";
+
+import ScheduleScreen from "./ScheduleScreen";
+import Colors from "./assets/Colors";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ScheduleScreen />
+        <StatusBar style="auto" hidden={true} />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.background,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
